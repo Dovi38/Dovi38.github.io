8dbe6a00b7512633e268799621c06078bb80d263
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2022-04-10 19:11:46 +0100

    colors changed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const toggleMenu = document.querySelector(".toggle-menu");
 const navBar = document.querySelector(".navbar");
+const logo = document.querySelector("#logo");
 
 toggleMenu.addEventListener("click", () => {
-  navBar.classList.toggle("active");
+  //navBar.classList.toggle("active");
+  navBar.classList.add("active");
+});
+navBar.addEventListener("click", () => {
+  navBar.classList.remove("active");
+});
+logo.addEventListener("click", () => {
+  navBar.classList.remove("active");
 });
