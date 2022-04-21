ea078c3fd2c24f3dbbb99857af7f31f68301f6e3
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2022-04-21 21:44:14 +0100

    readme updated, code not in use deleted

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Dovi38.github.io
 
-This is my web developer portfolio.
+This is my web developer portfolio project. This project is build from scratch, using HTML, CSS, SCSS, JS.
--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const toggleMenu = document.querySelector(".toggle-menu");
 const navBar = document.querySelector(".navbar");
 const logo = document.querySelector("#logo");
-const submitButton = document.querySelector(".form__box__button");
-const formContact = document.querySelector("#contactForm");
-console.log(formContact);
-const textInput = document.querySelector("#message");
 
 toggleMenu.addEventListener("click", () => {
-  //navBar.classList.toggle("active");
   navBar.classList.add("active");
 });
 navBar.addEventListener("click", () => {
@@ -16,10 +11,3 @@ navBar.addEventListener("click", () => {
 logo.addEventListener("click", () => {
   navBar.classList.remove("active");
 });
-
-// formContact.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   let text = (textInput.value = "");
-//   formContact.reset();
-//   console.log(text);
-// });
